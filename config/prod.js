aaf626868acd848718cb5b929d35b06cb136c1be
TAKAHIRO-webdev <tk30.webdeveloper@gmail.com>
2019-11-04 08:50:05 +0900

    Modified prod.js

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MongoDB: process.env.MONGO_DB,
+  FacebookAppID: process.env.FB_APP_ID,
+  FacebookAppSecret: process.env.FB_APP_SECRET,
+  GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+  GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+};
