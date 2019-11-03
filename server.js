f28b48e9bb80dfd7fc382a0d8d66959930a500d7
TAKAHIRO-webdev <tk30.webdeveloper@gmail.com>
2019-11-04 02:15:23 +0900

    Finished Facebook Auth

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const User = require('./models/user');
 const app = express();
 // load keys file
 const keys = require('./config/keys');
+// Load Helpers
+const {requireLogin,ensureGuest} = require('./helpers/auth');
 // use body parser middleware
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -23,6 +25,11 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+// Make user global object
+app.use((req,res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
 // load facebook strategy
 require('./passport/facebook');
 // connect to mLab MongoDB
@@ -38,19 +45,19 @@ app.engine('handlebars', exphbs({defaultlayout:'main'}));
 app.set('view engine', 'handlebars');
 
 
-app.get('/', (req,res) => {
+app.get('/',ensureGuest,(req,res) => {
   res.render('home', {
     title: 'Home'
   });
 });
 
-app.get('/about', (req,res) => {
+app.get('/about',ensureGuest, (req,res) => {
   res.render('about', {
     title:'About'
   });
 });
 
-app.get('/contact', (req,res) => {
+app.get('/contact',ensureGuest, (req,res) => {
   res.render('contact', {
     title: 'Contact'
   });
@@ -63,16 +70,41 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook',{
     successRedirect: '/profile',
     failureRedirect: '/'
 }));
-app.get('/profile',(req,res) => {
+app.get('/profile',requireLogin,(req,res) => {
     User.findById({_id:req.user._id}).then((user) => {
-        if (user) {
-            res.render('profile',{
-                title: 'Profile',
-                user:user
-            });
-        }
+          if (user) {
+              user.online = true;
+              user.save((err, user) => {
+                  if (err) {
+                      throw err;
+                  }else{
+                      res.render('profile',{
+                          title: 'Profile',
+                          user:user
+                        });
+                  }
+              });
+        } 
     });
 });
+
+app.get('/logout', (req,res) => {
+    User.findById({_id:req.user._id})
+    .then((user) => {
+        user.online = false;
+        user.save((err,user) => {
+          if (err) {
+              throw err;
+          }
+          if (user) {
+            req.logout();
+            res.redirect('/');
+          }
+        })
+    })
+
+});
+
 app.post('/contactUs', (req,res) => {
   console.log(req.body);
   const newMessage = {
