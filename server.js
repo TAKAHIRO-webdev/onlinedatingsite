3810c79fb27e0299ba2d4b054b0c4ce532a89c76
TAKAHIRO-webdev <tk30.webdeveloper@gmail.com>
2019-11-03 10:07:39 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//setup view engine
+app.engine('handlebars', exphbs({defaultlayout:'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req,res) => {
+  res.render('home');
+});
+
+app.get('/about', (req,res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req,res) => {
+  res.render('contact');
+});
+
+app.listen(port, ()=> {
+  console.log(`Server is running on port ${port}`);
+});
