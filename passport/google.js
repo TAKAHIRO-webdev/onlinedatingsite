7c17255cd4251778e5dd5b3c67f8e9eb3b9fa219
TAKAHIRO-webdev <tk30.webdeveloper@gmail.com>
2019-11-04 09:06:23 +0900

    google auth updated

--- passport/google.js
+++ passport/google.js
@@ -16,7 +16,7 @@ passport.deserializeUser((id,done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.GoogleClientID,
     clientSecret: keys.GoogleClientSecret,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'https://fierce-island-05808.herokuapp.com/auth/google/callback'
 },(accessToken, refreshToken, profile, done) => {
     console.log(profile);
 
