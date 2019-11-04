a727db94e2566ef7896adaa7bf9ae1c83d45e275
TAKAHIRO-webdev <tk30.webdeveloper@gmail.com>
2019-11-04 09:01:56 +0900

    fb callback url updated

--- passport/facebook.js
+++ passport/facebook.js
@@ -16,7 +16,7 @@ passport.deserializeUser((id,done) => {
 passport.use(new FacebookStrategy({
     clientID: keys.FacebookAppID,
     clientSecret: keys.FacebookAppSecret,
-    callbackURL: 'https://fierce-island-05808.herokuapp.com/auth/facebook/callback',
+    callbackURL: 'https://fierce-island-05808.herokuapp.com /auth/facebook/callback',
     profileFields: ['email', 'name', 'displayName','photos']
 },(accessToken, refreshToken, profile,done) => {
     console.log(profile);
